052e84d105a2306298cdee8a85ea9dfc5aed2915
LiuYan <361112237@qq.com>
2021-09-02 14:32:50 +0800

    feat(nav): hide nav in small screen

--- src/layouts/default/index.vue
+++ src/layouts/default/index.vue
@@ -16,32 +16,48 @@ import AppFooter from './footer/index.vue'
 import AppHeader from './header/index.vue'
 import AppSider from './sider/index.vue'
 import WindowManager from '@/layouts/components/WindowManager/index.vue'
-export default {
+import { useRoute } from 'vue-router'
+import {defineComponent, ref, watch, provide} from 'vue'
+
+export default defineComponent({
+  setup() {
+    const openSideBar = ref(false)
+    const route = useRoute()
+
+    provide('openSideBar', openSideBar)
+
+    watch(() => route.path, () => {
+      openSideBar.value = false
+    })
+  },
   components: {
     AppFooter,
     AppHeader,
     AppSider,
     WindowManager
   }
-}
+})
 </script>
 <style>
 .autok3s-ui__root {
   display: grid;
   height: 100vh;
-  grid-template-areas: "header header"
-                       "nav main"
-                       "wm wm";
+  grid-template-areas: "header"
+                       "main"
+                       "wm";
   grid-template-rows: var(--header-height) auto var(--wm-height, 0);
-  grid-template-columns: var(--nav-width) auto;
+  grid-template-columns: auto;
 }
 
 .autok3s-ui__header {
   grid-area: header;
 }
 .autok3s-ui__nav {
-  grid-area: nav;
-  overflow-y: auto;
+  position: fixed;
+  height: 100%;
+  width: var(--nav-width);
+  background-color: #fff;
+  z-index: var(--nav-z-index);
 }
 .autok3s-ui__wm {
   grid-area: wm;
@@ -53,4 +69,17 @@ export default {
   grid-template-rows: 1fr var(--footer-height);
 }
 
+@screen lg {
+  .autok3s-ui__root {
+    grid-template-areas: "header header"
+                         "nav main"
+                         "wm wm";
+    grid-template-columns: var(--nav-width) auto;
+  }
+  .autok3s-ui__nav {
+    grid-area: nav;
+    position: static;
+    z-index: 0;
+  }
+}
 </style>
--- src/layouts/default/sider/index.vue
+++ src/layouts/default/sider/index.vue
@@ -1,33 +1,53 @@
 <template>
-  <nav class="k-app-nav border-r overflow-y-auto">
-    <nav-group v-for="g in routeGroups" :key="g.name"
+  <nav class="k-app-nav border-r h-full flex flex-col transform lg:transform-none -translate-x-full" :class="{'-translate-x-0': openSideBar}">
+    <div class="lg:hidden h-$header-height grid grid-cols-[auto,auto,1fr] items-center gap-x-10px py-0 px-5px">
+      <k-icon type="arrow-right" direction="left" class="cursor-pointer" :size="24" @click="toggleSideBar"></k-icon>
+      <img class="h-32px object-contain" :src="k3sLogo">
+      <div class="overflow-ellipsis">{{currentGroup.title}}</div>
+    </div>
+    <div class="overflow-y-auto flex-grow">
+      <nav-group v-for="g in routeGroups" :key="g.name"
       :routeGroup="g"
       :expanded-state-map="stateMap"
       @toggle-open="toggleOpenState($event)"
       >
       </nav-group>
+    </div>
   </nav>
 </template>
 <script>
 import NavGroup from "./NavGroup.vue"
 import useRouteGroups from '../composables/useRouteGroups.js'
 import useRouteGroupState from '../composables/useRouteGroupState.js'
-import { computed } from 'vue'
-export default {
+import KIcon from '@/components/Icon'
+import { computed, defineComponent, inject } from 'vue'
+import k3sLogo from '@/assets/k3s.svg'
+
+export default defineComponent({
   setup() {
     const { routeGroups: rootRouteGroups, currentGroup } = useRouteGroups()
     const {stateMap, toggleOpenState} = useRouteGroupState(rootRouteGroups)
     const routeGroups = computed(() => {
       return currentGroup.value.children
     })
+    const openSideBar = inject('openSideBar')
+    const toggleSideBar = () => {
+     openSideBar.value = !openSideBar.value
+    }
+
     return {
       routeGroups,
       stateMap,
       toggleOpenState,
+      currentGroup,
+      toggleSideBar,
+      openSideBar,
+      k3sLogo,
     }
   },
   components: {
-    NavGroup
+    NavGroup,
+    KIcon,
   }  
-}
+})
 </script>
