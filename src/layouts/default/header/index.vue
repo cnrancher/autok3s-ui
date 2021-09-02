f465f73e5ce3ee723b953b70bdb56c0bfe5e1e00
LiuYan <361112237@qq.com>
2021-09-02 14:32:50 +0800

    feat(nav): hide nav in small screen

--- src/layouts/default/header/index.vue
+++ src/layouts/default/header/index.vue
@@ -1,25 +1,40 @@
 <template>
   <header class="grid grid-cols-[var(--nav-width),max-content] items-center justify-between pr-20px border-b bg-white">
-    <k-dropdown-nav :route-groups="routeGroups" :current-group="currentGroup"></k-dropdown-nav>
+    <!-- <k-dropdown-nav :route-groups="routeGroups" :current-group="currentGroup"></k-dropdown-nav> -->
+    <div class="grid grid-cols-[auto,auto,1fr] items-center h-full gap-x-10px py-0 px-5px">
+      <k-icon type="category" :size="24" class="lg:hidden cursor-pointer " @click="toggleSidBar"></k-icon>
+      <img class="h-32px object-contain" :src="k3sLogo">
+      <div class="overflow-ellipsis">{{currentGroup.title}}</div>
+    </div>
     <kube-ctl-button></kube-ctl-button>
   </header>
 </template>
 <script>
-import KDropdownNav from './DropdownNav.vue'
+// import KDropdownNav from './DropdownNav.vue'
 import KubeCtlButton from './KubeCtlButton.vue'
 import useRouteGroups from '../composables/useRouteGroups.js'
-export default {
-  setup() {
-    const { routeGroups, currentGroup } = useRouteGroups()
+import k3sLogo from '@/assets/k3s.svg'
+import KIcon from '@/components/Icon'
+import {defineComponent, inject} from 'vue'
+export default defineComponent({
+  setup(props, { emit }) {
+    const { currentGroup } = useRouteGroups()
+    const openSideBar = inject('openSideBar')
+
+    const toggleSidBar = () => {
+     openSideBar.value = !openSideBar.value
+    }
     return {
-      routeGroups,
-      currentGroup
+      currentGroup,
+      k3sLogo,
+      toggleSidBar,
     }
   },
   components: {
-    KDropdownNav,
+    // KDropdownNav,
     KubeCtlButton,
+    KIcon,
   }
-}
+})
 </script>
 
