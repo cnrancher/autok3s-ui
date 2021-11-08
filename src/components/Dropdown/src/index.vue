d749decd5490500856aabddba8fa5780986f13fa
LiuYan <361112237@qq.com>
2021-11-08 16:24:20 +0800

    fix(dropdown): fix clicking on an item is unresponsive

--- src/components/Dropdown/src/index.vue
+++ src/components/Dropdown/src/index.vue
@@ -56,7 +56,7 @@ export default defineComponent({
       create()
       update()
     }
-    onClickOutside(toggleRef, () => {
+    onClickOutside(contentRef, () => {
       show.value = false
     })
     watch(show, () => {
