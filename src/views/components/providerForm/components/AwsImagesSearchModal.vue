2b0354f4f7d8ab7fa6a280cc41f30f3cf677cac9
LiuYan <361112237@qq.com>
2022-09-07 15:42:26 +0800

    feat(AwsImagesSearchModal): support for secondary filtering of results

--- src/views/components/providerForm/components/AwsImagesSearchModal.vue
+++ src/views/components/providerForm/components/AwsImagesSearchModal.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, toRef, watch } from 'vue'
+import { computed, ref, toRef, watch } from 'vue'
 import usePagination from '@/composables/usePagination.js'
 import Pagination from '@/components/Pagination'
 const props = defineProps({
@@ -33,7 +33,86 @@ const owners = ref([...props.imageInfo.owners]) ?? ref(['self', 'amazon', 'aws-m
 const field = ref(props.imageInfo.field) ?? ref('name')
 const fields = ref(['name', 'description', 'image-id'])
 
-const { pageData, currentPage, total, pageSize } = usePagination(data)
+const archQuery = ref('')
+const platformQuery = ref('')
+const ownerQuery = ref('')
+
+watch(data, () => {
+  archQuery.value = ''
+  platformQuery.value = ''
+  ownerQuery.value = ''
+})
+
+const sortFn = (a, b) => {
+  if (a.value > b.value) {
+    return 1
+  }
+  if (a.value < b.value) {
+    return -1
+  }
+  return 0
+}
+
+const resultArch = computed(() => {
+  const r = data.value.reduce((t, c) => {
+    if (c.Architecture && !t.find((item) => item.value === c.Architecture)) {
+      t.push({ value: c.Architecture, label: c.Architecture })
+    }
+    return t
+  }, [])
+  r.sort(sortFn)
+  r.push({ value: '', label: 'All' })
+  return r
+})
+const resultPlatforms = computed(() => {
+  const r = data.value.reduce((t, c) => {
+    if (c.PlatformDetails && !t.find((item) => item.value === c.PlatformDetails)) {
+      t.push({ value: c.PlatformDetails, label: c.PlatformDetails })
+    }
+
+    return t
+  }, [])
+  r.sort(sortFn)
+  r.push({ value: '', label: 'All' })
+  return r
+})
+const resultOwners = computed(() => {
+  const r = data.value.reduce((t, c) => {
+    if (c.ImageOwnerAlias && !t.find((item) => item.value === c.ImageOwnerAlias)) {
+      t.push({ value: c.ImageOwnerAlias, label: c.ImageOwnerAlias })
+    }
+
+    return t
+  }, [])
+  r.sort(sortFn)
+  r.push({ value: '', label: 'All' })
+  return r
+})
+
+const result = computed(() => {
+  const filters = [
+    {
+      name: 'Architecture',
+      value: archQuery.value
+    },
+    {
+      name: 'PlatformDetails',
+      value: platformQuery.value
+    },
+    {
+      name: 'ImageOwnerAlias',
+      value: ownerQuery.value
+    }
+  ].filter((item) => item.value)
+
+  if (filters.length === 0) {
+    return data.value
+  }
+
+  return data.value.filter((item) => filters.every((f) => item[f.name] === f.value))
+})
+
+const { pageData, currentPage, total, pageSize } = usePagination(result)
 pageSize.value = 10
 const searchQuery = ref(query)
 const arch = ref([...defaultArch])
@@ -84,8 +163,8 @@ watch(
           />
 
           <KButton :disabled="imageInfo.loading" class="role-primary self-start" @click="searchImages">Query</KButton>
-          <div class="col-span-3 grid gap-4 grid-cols-[auto,auto,auto,1fr] justify-self-start">
-            Architecture:
+          <div class="col-span-3 grid gap-4 grid-cols-[auto,auto,auto,1fr] justify-self-start items-center">
+            <span>Architecture:</span>
             <label>
               <input v-model="arch" type="checkbox" value="i386" />
               i386
@@ -99,8 +178,8 @@ watch(
               arm64
             </label>
           </div>
-          <div class="col-span-3 grid gap-4 grid-cols-[auto,auto,auto,1fr] justify-self-start">
-            Owners:
+          <div class="col-span-3 grid gap-4 grid-cols-[auto,auto,auto,1fr] justify-self-start items-center">
+            <span>Owners:</span>
             <label>
               <input v-model="owners" type="checkbox" value="self" />
               Self
@@ -114,8 +193,8 @@ watch(
               AWS Marketplace
             </label>
           </div>
-          <div class="col-span-3 grid gap-y-2 gap-x-4 grid-cols-[auto,auto,auto,1fr] justify-self-start">
-            Virtualization Type
+          <div class="col-span-3 grid gap-y-2 gap-x-4 grid-cols-[auto,auto,auto,1fr] justify-self-start items-center">
+            <span>Virtualization Type:</span>
             <label>
               <input v-model="virtualizationType" type="checkbox" value="hvm" />
               HVM
@@ -141,29 +220,60 @@ watch(
           </div> -->
         </div>
         <div v-if="imageInfo.error">{{ imageInfo.error }}</div>
-        <div class="grid gap-4">
-          <div
-            v-for="d in pageData"
-            :key="d.ImageId"
-            class="odd:bg-gray-50 even:bg-light-50 p-y-4 grid grid-cols-[1fr,max-content] items-center"
-          >
+        <div class="grid grid-cols-[200px,1fr] items-start gap-2">
+          <div v-show="data.length > 0">
             <div>
-              <div>Image ID: {{ d.ImageId }}</div>
-              <div>Name: {{ d.Name }}</div>
-              <div>Description: {{ d.Description }}</div>
-              <div>Architecture: {{ d.Architecture }}</div>
+              <div class="text-lg">Architecture:</div>
+              <div class="flex flex-wrap gap-2">
+                <label v-for="a in resultArch" :key="a.label" class="hover:bg-gray-100">
+                  <input v-model="archQuery" type="radio" :value="a.value" />
+                  {{ a.label }}
+                </label>
+              </div>
             </div>
-            <KButton
-              class="role-primary row-span-2"
-              @click="
-                () => {
-                  $emit('select', d)
-                  $emit('close')
-                }
-              "
+            <div>
+              <div class="text-lg">Platform:</div>
+              <div class="flex flex-wrap gap-2">
+                <label v-for="a in resultPlatforms" :key="a.label" class="hover:bg-gray-100">
+                  <input v-model="platformQuery" type="radio" :value="a.value" />
+                  {{ a.label }}
+                </label>
+              </div>
+            </div>
+            <div>
+              <div class="text-lg">Owner:</div>
+              <div class="flex flex-wrap gap-2">
+                <label v-for="a in resultOwners" :key="a.label" class="hover:bg-gray-100">
+                  <input v-model="ownerQuery" type="radio" :value="a.value" />
+                  {{ a.label }}
+                </label>
+              </div>
+            </div>
+          </div>
+          <div class="grid gap-4">
+            <div
+              v-for="d in pageData"
+              :key="d.ImageId"
+              class="odd:bg-gray-50 even:bg-light-50 p-y-2 grid grid-cols-[1fr,max-content] items-center gap-2"
             >
-              Choose
-            </KButton>
+              <div class="hover:bg-gray-100">
+                <div>Image ID: {{ d.ImageId }}</div>
+                <div>Name: {{ d.Name }}</div>
+                <div>Description: {{ d.Description }}</div>
+                <div>Architecture: {{ d.Architecture }}</div>
+              </div>
+              <KButton
+                class="role-primary row-span-2"
+                @click="
+                  () => {
+                    $emit('select', d)
+                    $emit('close')
+                  }
+                "
+              >
+                Choose
+              </KButton>
+            </div>
           </div>
         </div>
         <Pagination v-model:current-page="currentPage" :total="total" :page-size="pageSize"></Pagination>
