e0c4e6b4d51ad5406c89a13e4e63f0f01850d698
LiuYan <361112237@qq.com>
2022-11-23 15:42:33 +0800

    feat(providerForm): dynamic load provider form

--- src/views/components/providerForm/hooks/useProviderAsyncForm.js
+++ src/views/components/providerForm/hooks/useProviderAsyncForm.js
@@ -1,13 +1,21 @@
 import { defineAsyncComponent } from 'vue'
-const asyncComponents = {
-  alibaba: defineAsyncComponent(() => import('../AlibabaClusterForm.vue')),
-  aws: defineAsyncComponent(() => import('../AwsClusterForm.vue')),
-  google: defineAsyncComponent(() => import('../GoogleClusterForm.vue')),
-  harvester: defineAsyncComponent(() => import('../HarvesterClusterForm.vue')),
-  k3d: defineAsyncComponent(() => import('../K3dClusterForm.vue')),
-  native: defineAsyncComponent(() => import('../NativeClusterForm.vue')),
-  tencent: defineAsyncComponent(() => import('../TencentClusterForm.vue'))
-}
+// const asyncComponents = {
+//   alibaba: defineAsyncComponent(() => import('../AlibabaClusterForm.vue')),
+//   aws: defineAsyncComponent(() => import('../AwsClusterForm.vue')),
+//   google: defineAsyncComponent(() => import('../GoogleClusterForm.vue')),
+//   harvester: defineAsyncComponent(() => import('../HarvesterClusterForm.vue')),
+//   k3d: defineAsyncComponent(() => import('../K3dClusterForm.vue')),
+//   native: defineAsyncComponent(() => import('../NativeClusterForm.vue')),
+//   tencent: defineAsyncComponent(() => import('../TencentClusterForm.vue'))
+// }
+
+const files = import.meta.glob('../*ClusterForm.vue')
+const asyncComponents = Object.entries(files).reduce((t, [k, v]) => {
+  const p = k.substring(3, k.indexOf('ClusterForm.vue')).toLowerCase()
+  t[p] = defineAsyncComponent(v)
+
+  return t
+}, {})
 
 export default function useProviderAsyncForm() {
   return asyncComponents
