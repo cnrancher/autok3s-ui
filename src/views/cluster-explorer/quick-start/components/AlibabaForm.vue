9f6870fd9dc415a57ef2e1ddff16866f5b5e9f91
LiuYan <361112237@qq.com>
2023-01-09 16:22:35 +0800

    fix(quick-start): support select credential

--- src/views/cluster-explorer/quick-start/components/AlibabaForm.vue
+++ src/views/cluster-explorer/quick-start/components/AlibabaForm.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="grid grid-cols-1 sm:grid-cols-2 gap-20px">
+    <CredentialSelectForm
+      v-show="showKeyForm"
+      v-model="credentialValue"
+      class="sm:col-span-2"
+      provider="alibaba"
+      required
+      label="Alibaba Credential"
+      :desc="desc"
+    />
     <string-form v-model.trim="form.config['master']" label="Master" :desc="desc.config['master']" />
     <string-form v-model.trim="form.config['worker']" label="Worker" :desc="desc.config['worker']" />
-    <k-password-input
+    <!-- <k-password-input
       v-show="showKeyForm"
       v-model.trim="form.options['access-key']"
       label="Access Key"
@@ -15,7 +24,8 @@
       label="Access Secret"
       :desc="desc.options['access-secret']"
       required
-    />
+    /> -->
+
     <string-form v-model.trim="form.options.region" label="Region" :desc="desc.options.region" disabled />
     <string-form v-model.trim="form.options.zone" label="Zone" :desc="desc.options.zone" disabled />
   </div>
@@ -25,6 +35,7 @@ import { computed, watch, reactive } from 'vue'
 import StringForm from '@/views/components/baseForm/StringForm.vue'
 import { cloneDeep } from '@/utils'
 import useFormRegist from '@/composables/useFormRegist.js'
+import CredentialSelectForm from '@/views/components/baseForm/CredentialSelectForm.vue'
 
 const props = defineProps({
   initValue: {
@@ -50,6 +61,18 @@ watch(
 )
 const showKeyForm = computed(() => {
   return props.hasError || !props.initValue.options['access-key'] || !props.initValue.options['access-secret']
+})
+const credentialValue = computed({
+  get() {
+    return {
+      'access-key': form.options['access-key'],
+      'access-secret': form.options['access-secret']
+    }
+  },
+  set(v) {
+    form.options['access-key'] = v['access-key']
+    form.options['access-secret'] = v['access-secret']
+  }
 })
 const getForm = () => {
   const f = cloneDeep(form)
--- src/views/cluster-explorer/quick-start/components/TencentForm.vue
+++ src/views/cluster-explorer/quick-start/components/TencentForm.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="grid grid-cols-1 sm:grid-cols-2 gap-20px">
+    <CredentialSelectForm
+      v-show="showKeyForm"
+      v-model="credentialValue"
+      class="sm:col-span-2"
+      provider="tencent"
+      required
+      label="Tencent Credential"
+      :desc="desc"
+    />
     <string-form v-model.trim="form.config['master']" label="Master" :desc="desc.config['master']" />
     <string-form v-model.trim="form.config['worker']" label="Worker" :desc="desc.config['worker']" />
-    <k-password-input
+    <!-- <k-password-input
       v-show="showKeyForm"
       v-model.trim="form.options['secret-id']"
       label="Secret Id"
@@ -15,7 +24,7 @@
       label="Secret Key"
       :desc="desc.options['secret-key']"
       required
-    />
+    /> -->
     <string-form v-model.trim="form.options.region" label="Region" :desc="desc.options.region" disabled />
     <string-form v-model.trim="form.options.zone" label="Zone" :desc="desc.options.zone" disabled />
   </div>
@@ -25,6 +34,7 @@ import { computed, watch, reactive } from 'vue'
 import StringForm from '@/views/components/baseForm/StringForm.vue'
 import { cloneDeep } from '@/utils'
 import useFormRegist from '@/composables/useFormRegist.js'
+import CredentialSelectForm from '@/views/components/baseForm/CredentialSelectForm.vue'
 
 const props = defineProps({
   initValue: {
@@ -50,6 +60,18 @@ watch(
 )
 const showKeyForm = computed(() => {
   return props.hasError || !props.initValue.options['secret-id'] || !props.initValue.options['secret-key']
+})
+const credentialValue = computed({
+  get() {
+    return {
+      'secret-id': form.options['secret-id'],
+      'secret-key': form.options['secret-key']
+    }
+  },
+  set(v) {
+    form.options['secret-id'] = v['secret-id']
+    form.options['secret-key'] = v['secret-key']
+  }
 })
 const getForm = () => {
   const f = cloneDeep(form)
