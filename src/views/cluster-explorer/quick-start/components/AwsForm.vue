e75dafa88df16f833acf569821e54641de712a1b
LiuYan <361112237@qq.com>
2023-01-09 16:22:35 +0800

    fix(quick-start): support select credential

--- src/views/cluster-explorer/quick-start/components/AwsForm.vue
+++ src/views/cluster-explorer/quick-start/components/AwsForm.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="grid grid-cols-1 sm:grid-cols-2 gap-20px">
+    <CredentialSelectForm
+      v-show="showKeyForm"
+      v-model="credentialValue"
+      class="sm:col-span-2"
+      provider="aws"
+      required
+      label="AWS Credential"
+      :desc="desc"
+    />
     <string-form v-model.trim="form.config['master']" label="Master" :desc="desc.config['master']" />
     <string-form v-model.trim="form.config['worker']" label="Worker" :desc="desc.config['worker']" />
-    <k-password-input
+    <!-- <k-password-input
       v-show="showKeyForm"
       v-model.trim="form.options['access-key']"
       label="Access Key"
@@ -15,7 +24,7 @@
       label="Secret Key"
       :desc="desc.options['secret-key']"
       required
-    />
+    /> -->
     <string-form v-model.trim="form.options.region" label="Region" :desc="desc.options.region" disabled />
     <string-form v-model.trim="form.options.zone" label="Zone" :desc="desc.options.zone" disabled />
   </div>
@@ -25,6 +34,7 @@ import { computed, watch, reactive } from 'vue'
 import StringForm from '@/views/components/baseForm/StringForm.vue'
 import { cloneDeep } from '@/utils'
 import useFormRegist from '@/composables/useFormRegist.js'
+import CredentialSelectForm from '@/views/components/baseForm/CredentialSelectForm.vue'
 
 const props = defineProps({
   initValue: {
@@ -49,6 +59,18 @@ watch(
 )
 const showKeyForm = computed(() => {
   return props.hasError || !props.initValue.options['access-key'] || !props.initValue.options['secret-key']
+})
+const credentialValue = computed({
+  get() {
+    return {
+      'access-key': form.options['access-key'],
+      'secret-key': form.options['secret-key']
+    }
+  },
+  set(v) {
+    form.options['access-key'] = v['access-key']
+    form.options['secret-key'] = v['secret-key']
+  }
 })
 const getForm = () => {
   const f = cloneDeep(form)
--- src/views/cluster-explorer/quick-start/components/GoogleForm.vue
+++ src/views/cluster-explorer/quick-start/components/GoogleForm.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="grid grid-cols-1 sm:grid-cols-2 gap-20px">
+    <CredentialSelectForm
+      v-show="showKeyForm"
+      v-model="credentialValue"
+      class="sm:col-span-2"
+      provider="google"
+      required
+      label="GCE Credential"
+      :desc="desc"
+      @credential-change="handleCredentialChange"
+    />
     <string-form v-model.trim="form.config['master']" label="Master" :desc="desc.config['master']" />
     <string-form v-model.trim="form.config['worker']" label="Worker" :desc="desc.config['worker']" />
-    <string-form
+    <!-- <string-form
       v-show="showKeyForm"
       v-model.trim="form.options['service-account']"
       label="Service Account"
@@ -15,7 +25,7 @@
       label="Service Account File"
       :desc="desc.options['service-account-file']"
       required
-    />
+    /> -->
     <string-form v-model.trim="form.options['project']" label="Project" :desc="desc.options['project']" required />
     <string-form v-model.trim="form.options.region" label="Region" :desc="desc.options.region" disabled />
     <string-form v-model.trim="form.options.zone" label="Zone" :desc="desc.options.zone" disabled />
@@ -26,6 +36,7 @@ import { computed, watch, reactive } from 'vue'
 import StringForm from '@/views/components/baseForm/StringForm.vue'
 import { cloneDeep } from '@/utils'
 import useFormRegist from '@/composables/useFormRegist.js'
+import CredentialSelectForm from '@/views/components/baseForm/CredentialSelectForm.vue'
 
 const props = defineProps({
   initValue: {
@@ -53,6 +64,18 @@ const showKeyForm = computed(() => {
   return (
     props.hasError || !props.initValue.options['service-account'] || !props.initValue.options['service-account-file']
   )
+})
+const credentialValue = computed({
+  get() {
+    return {
+      'service-account': form.options['service-account'],
+      'service-account-file': form.options['service-account-file']
+    }
+  },
+  set(v) {
+    form.options['service-account'] = v['service-account']
+    form.options['service-account-file'] = v['service-account-file']
+  }
 })
 const getForm = () => {
   const f = cloneDeep(form)
