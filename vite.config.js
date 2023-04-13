946c850b42e926d9988a8e08a63d3943bdab2eae
LiuYan <361112237@qq.com>
2023-04-13 10:10:22 +0800

    chore(test): update test deps

--- vite.config.js
+++ vite.config.js
@@ -60,7 +60,11 @@ export default ({ command, mode }) => {
       outDir: 'dist/static'
     },
     test: {
-      environment: 'jsdom' // or 'jsdom', 'node'
+      // 启用类似 jest 的全局测试 API
+      globals: true,
+      // 使用 happy-dom 模拟 DOM
+      // 这需要你安装 happy-dom 作为对等依赖（peer dependency）
+      environment: 'happy-dom'
     }
   }
 }
