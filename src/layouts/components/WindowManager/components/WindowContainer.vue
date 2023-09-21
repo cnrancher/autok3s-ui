8f452ae782a3b4747eedadf89d5116bc40ae9743
LiuYan <361112237@qq.com>
2023-09-21 17:06:45 +0800

    fix(log/shell window): fix log/shell window size error

--- src/layouts/components/WindowManager/components/WindowContainer.vue
+++ src/layouts/components/WindowManager/components/WindowContainer.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="grid grid-rows-[1fr,auto] h-full">
-    <div class="p-2px overflow-auto">
-      <slot></slot>
-    </div>
-    <div class="grid grid-flow-col justify-between items-center py-2px px-5px bg-gray-300 border-t">
-      <slot name="footer"></slot>
+  <div class="h-full">
+    <div class="grid grid-rows-[1fr_auto] h-full">
+      <div class="p-2px overflow-auto">
+        <slot></slot>
+      </div>
+      <div class="grid grid-flow-col justify-between items-center py-2px px-5px bg-gray-300 border-t">
+        <slot name="footer"></slot>
+      </div>
     </div>
   </div>
 </template>
